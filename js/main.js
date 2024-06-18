4ab3f15dcf2f2be602c77769485f3195af4a7bf8
robert <roberts.shalajevs@gmail.com>
2024-06-18 18:39:47 +0300

    a

--- js/main.js
+++ js/main.js
@@ -1,34 +1,57 @@
 const swiper = new Swiper(".swiper", {
-    effect: "fade",
-    pagination: {
-      el: ".swiper-pagination",
-    },
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
-  });
+  effect: "fade",
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+});
 
-  const tabItem = document.querySelectorAll('.tabs__btn-item');
-  const tabContent = document.querySelectorAll('.tabs__content-item');
+const tabItem = document.querySelectorAll('.tabsbtn-item');
+const tabContent = document.querySelectorAll('.tabscontent-item');
 
-  tabItem.forEach(function (element) {
-    element.addEventListener('click', open);
-  });
+tabItem.forEach(function (element) {
+  element.addEventListener('click', open);
+});
 
-  function open(evt) {
-    const tabTarget = evt.currentTarget;
-    const tabTargetIndex = Array.from(tabItem).indexOf(tabTarget);
-
-    tabItem.forEach(function(item) {
-      item.classList.remove('tabs__btn-item--active');
-    });
-    tabContent.forEach(function(content) {
-      content.classList.remove('tabs__content-item--active');
-    });
-
-    tabTarget.classList.add('tabs__btn-item--active');
-    tabContent[tabTargetIndex].classList.add('tabs__content-item--active');
-
+  // Function to open modal
+function openModal(modalId) {
+var modal = document.getElementById(modalId);
+modal.style.display = "block";
 }
 
+// Function to close modal
+function closeModal(modalId) {
+var modal = document.getElementById(modalId);
+modal.style.display = "none";
+}
+
+// Close modal if user clicks outside of it
+window.onclick = function(event) {
+var modals = document.getElementsByClassName('modal');
+for (var i = 0; i < modals.length; i++) {
+  if (event.target == modals[i]) {
+    modals[i].style.display = "none";
+  }
+}
+}
+
+function open(evt) {
+  const tabTarget = evt.currentTarget;
+  const tabTargetIndex = Array.from(tabItem).indexOf(tabTarget);
+
+  tabItem.forEach(function(item) {
+    item.classList.remove('tabsbtn-item--active');
+  });
+  tabContent.forEach(function(content) {
+    content.classList.remove('tabscontent-item--active');
+  });
+
+  tabTarget.classList.add('tabsbtn-item--active');
+  tabContent[tabTargetIndex].classList.add('tabscontent-item--active');
+
+
+
+}
--- main.py
+++ main.py
@@ -1,21 +1,33 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, session, make_response
 from flask_cors import CORS
+from flask_pymongo import PyMongo
 from pymongo import MongoClient
+import bcrypt
+import jwt
+import datetime
+import os
+import secrets
 from bson import json_util, ObjectId
+import json
 import random
-from flask import Flask, request, json
-from bson import json_util
-from pymongo import MongoClient
 import string
 
 app = Flask(__name__)
 CORS(app)
-
+app.secret_key = secrets.token_hex(24)
 client = MongoClient("mongodb+srv://davidkanin18:0909D.KANIN@cluster0.vbhzniv.mongodb.net/")
 
 db = client.DMleasing
 cars = db.cars
 contact = db.contact
+
+users = db.users
+tokenlist = db.tokenlist
+
+def generate_token(email):
+    token = jwt.encode({'email': email, 'exp': datetime.datetime.utcnow() + datetime.timedelta(days=1)}, app.secret_key, algorithm='HS256')
+    tokenlist.insert_one({'token': token})
+    return token
 
 @app.route('/cars', methods=['GET'])
 def get_orders():
@@ -37,6 +49,59 @@ def contact_handler():
         contact_data = request.json
         contact.insert_one(contact_data)
         return json_util.dumps(contact_data)
+
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    existing_user = users.find_one({'email': data['email']})
+
+    if existing_user:
+        return jsonify({'message': 'User already exists'}), 400
+
+    hashed_password = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt())
+    user = {
+        'email': data['email'],
+        'password': hashed_password,
+    }
+    users.insert_one(user)
+
+    new_user = users.find_one({'email': data['email']})
+    email = new_user['email']
+
+    token = jwt.encode({'email': data['email'], 'exp': datetime.datetime.utcnow() + datetime.timedelta(days=1)}, app.secret_key, algorithm='HS256')
+    tokenlist.insert_one({'token': token})
+
+    response = make_response(jsonify({'message': 'User registered successfully', 'email': email}), 201)
+    response.set_cookie('token', token, httponly=True, samesite='Strict')
+
+    return response
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    user = users.find_one({'email': data['email']})
+
+    if user and bcrypt.checkpw(data['password'].encode('utf-8'), user['password']):
+        token = jwt.encode({'email': data['email'], 'exp': datetime.datetime.utcnow() + datetime.timedelta(days=1)}, app.secret_key, algorithm='HS256')
+        tokenlist.insert_one({'token': token})
+
+        response = make_response(jsonify({'message': 'Login successful', 'email': data['email'], 'token': token}))
+        response.set_cookie('token', token, httponly=True, samesite='Strict')
+
+        return response
+    else:
+        return jsonify({'message': 'Invalid credentials'}), 401
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    token = request.cookies.get('token')
+    if token:
+        tokenlist.delete_one({'token': token})
+        response = make_response(jsonify({'message': 'Logged out successfully'}), 200)
+        response.delete_cookie('token')
+        return response
+    else:
+        return jsonify({'message': 'Token missing'}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
